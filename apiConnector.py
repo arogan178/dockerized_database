4e45cfffef5dc0a585fccd4b0c03d55b11fca179
arogan178 <andrea.bugeja@hotmail.com>
2023-03-05 13:55:51 +0100

    Find total amount of users and split into batches.

--- apiConnector.py
+++ apiConnector.py
@@ -1,42 +1,48 @@
 import requests
 
 # Define API endpoint and urls
-base_url = 'http://dummyapi.io/data/v1/'
+base_url = "http://dummyapi.io/data/v1/"
 users_url = f"{base_url}/user"
 users_profile_url = f"{base_url}/user/{{}}"
 
-headers = {'app-id': '6401c57240b15c493ba72bea'}
+headers = {"app-id": "6401c57240b15c493ba72bea"}
 
-# Params
-params = {'limit': 30}
+batch_size = 50
+
+response = requests.get(users_url, headers=headers)
+total_users = response.json()["total"]
+print(f"Total number of users: {total_users}")
+
+
+# Calculate the number of page iterations required
+pages = (total_users + batch_size - 1) // batch_size
+print(f"Number of pages: {pages}")
 
 # This will store all male users from request
 male_users = []
 
-response = requests.get(users_url, headers=headers, params={"limit": 0})
-total_users = response.json()["total"]
+for page in range(pages):
+    print(f"Page: {page}")
+    params = {"limit": batch_size, "page": page}
+    users_response = requests.get(users_url, headers=headers, params=params)
+    users = users_response.json()["data"]
+    for user in users:
+        # get profile of user
+        user_profile_response = requests.get(
+            f"{users_url}/{user['id']}", headers=headers)
+        user_profile = user_profile_response.json()
 
-for i in range(0, total_users, params["limit"]):
-    params["offset"] = i
-    response = requests.get(users_url, headers=headers, params=params)
-    users_data = response.json()["data"]
-
-    # Retrieve the full user profile for each user in the batch
-    for user in users_data:
-        user_details_response = requests.get(users_profile_url.format(user['id']), headers=headers)
-        user_details = user_details_response.json()
-
-        # Check if the user is male
-        if user_details['gender'] == 'male':
+        # check if male
+        if user_profile["gender"] == "male":
             user_info = {
-                'first_name': user_details['firstName'],
-                'last_name': user_details['lastName'],
-                'dob': user_details['dateOfBirth'],
-                'gender': user_details['gender'],
-                'name_character_count': len(user_details['firstName']) + len(user_details['lastName'])
+                "first_name": user_profile["firstName"],
+                "last_name": user_profile["lastName"],
+                "gender": user_profile["gender"],
+                "dob": user_profile["dateOfBirth"],
+                "name_char_count": len(user_profile["firstName"]) + len(user_profile["lastName"])
             }
-
+            print(
+                f"The name {user_info['first_name']} {user_info['last_name']} contains {user_info['name_char_count']} characters.")
             male_users.append(user_info)
 
-print(male_users)
-print(len(male_users))
+print(f"Males in total: {len(male_users)}")
